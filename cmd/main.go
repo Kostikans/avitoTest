5991dcbcd18ad15e0ec76cdf44ebe0060991f3b1
Kostikans <48389219+Kostikans@users.noreply.github.com>
2020-12-28 21:15:08 +0300

    projectStructure

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package cmd
+
+import "log"
+
+func InitDB() *sqlx.DB {
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		configs.BdConfig.User,
+		configs.BdConfig.Password,
+		configs.BdConfig.DBName)
+
+	fmt.Println(connStr)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
+func main(){
+
+}
