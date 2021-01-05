4e8e374e2ad65a0d6f70f2604f70f84f5d0c16a3
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-01-05 22:33:01 +0300

    update indexes

--- init.sql
+++ init.sql
@@ -12,8 +12,11 @@ create table booking
     room_id     int,
     date_start  timestamp,
     date_end    timestamp,
-    CONSTRAINT fk_rooms
-        FOREIGN KEY(room_id)
-            REFERENCES rooms(room_id)
-            ON DELETE CASCADE
+    CONSTRAINT fk_rooms FOREIGN KEY(room_id) REFERENCES rooms(room_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE INDEX booking_idx_1 ON booking(room_id);
+CREATE INDEX booking_idx_2 ON booking(booking_id);
+CREATE INDEX rooms_idx_1 ON rooms(room_id);
+CREATE INDEX booking_where__idx ON booking(room_id,booking_id,date_start,date_end); --index only scan
+
